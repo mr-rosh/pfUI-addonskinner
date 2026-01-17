48cd30bff231add81b7d892b4fc1c9d40912c57e
Dirty <Dirty@xyz.com>
2026-01-17 11:14:00 +0100

    Qucikheal skinning for Dirty-git:feature/aggro_based_precast

--- skins/QuickHeal.lua
+++ skins/QuickHeal.lua
@@ -58,7 +58,6 @@ pfUI:RegisterSkin("QuickHeal", "vanilla", function ()
         QuickHealConfig_NotificationChannelNameEditBox, -- -- skinEditBox ???
         MTListFrame,
         QH_Display_Tooltip,
-        
       }
       
       local function Hover(btn, dir)
@@ -123,6 +122,9 @@ pfUI:RegisterSkin("QuickHeal", "vanilla", function ()
         QuickHealConfig_CheckButtonOverhealMessagePlaySound,
         QuickHealConfig_CheckButtonTargetPriority,
         QuickHealConfig_CheckButtonQuickClick,
+        QuickHealConfig_CheckButtonStopcast,
+        QuickHealConfig_CheckButtonPrecastAggro,
+        QuickHealConfig_CheckButtonPreHOTAggro,
         QuickHealConfig_FilterRaidGroup1CheckBox,
         QuickHealConfig_FilterRaidGroup2CheckBox,
         QuickHealConfig_FilterRaidGroup3CheckBox,
@@ -141,6 +143,8 @@ pfUI:RegisterSkin("QuickHeal", "vanilla", function ()
         QuickHealConfig_RatioHealthySlider,
         QuickHealConfig_RatioForceselfSlider,
         QuickHealConfig_PetPrioritySlider,
+        QuickHealConfig_MaxOverhealPercentSlider,
+        QuickHealConfig_StopcastCheckWindowSlider,
         QuickHealDownrank_Slider_NH,
         QuickHealDownrank_Slider_FH,
         QuickHealMinrank_Slider_NH,
@@ -159,7 +163,7 @@ pfUI:RegisterSkin("QuickHeal", "vanilla", function ()
         SkinDropDown(dropdown)
       end
   
-      for i = 1, 3 do
+      for i = 1, 4 do
           local tab = _G["QuickHealConfigTab" .. i]
           if tab then
               SkinTab(tab)
